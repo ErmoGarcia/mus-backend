123f911f4311f31d6fdc517cc46788d1e7001e3c
Guillermo <g.ggrao@upm.es>
2023-01-19 01:44:20 +0100

    feat: register endpoint added

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ErmoGarcia/mus-backend/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	public := r.Group("/api")
+
+	public.POST("/register", controllers.Register)
+
+	r.Run(":8080")
+
 }
